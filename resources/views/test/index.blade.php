c48d4308bd6a73457ffdc70f1e488c34cf6cf224
Trotteriniku <stefanozidda3@gmail.com>
2023-12-22 17:23:53 +0100

    basic index layout

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+
+
+    <main>
+        <h1>Home page</h1>
+        <h1>Buone Feste</h1>
+        <h2>Hello </h2>
+        <h3>Lezzo</h3>
+    </main>
+
+
+
+
+@endsection
